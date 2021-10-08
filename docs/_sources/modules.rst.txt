9994b4c08f589ebeb01b05f8f92febb7f1bb0a59
urakubo <hurakubo@gmail.com>
2021-10-08 12:47:53 +0900

    Rearrange dirs

--- docs/_sources/modules.rst.txt
+++ docs/_sources/modules.rst.txt
@@ -1,5 +1,5 @@
-docs
-====
+Utilities
+=========
 
 .. automodule:: utils
    :members:
--- docs_src/conf.py
+++ docs_src/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('./pyLD'))
+sys.path.insert(0, os.path.abspath('../pyLD'))
 
 
 # -- Project information -----------------------------------------------------
