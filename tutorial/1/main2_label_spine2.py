482eb13836862951f4e597e0c6cc3bdac84f44f3
urakubo <hurakubo@gmail.com>
2021-11-14 10:50:46 +0900

    Docs

--- tutorial/1/main2_label_spine2.py
+++ tutorial/1/main2_label_spine2.py
@@ -10,13 +10,13 @@ output_image_file = 'imgs/labels_ball_and_stick.png'
 c = LoadLabeledVolume(input_label_file)
 
 mlab.figure(bgcolor=(1.0,1.0,1.0), size=(700,700))
-mlab.view(90, 90, 150, [ 50, 30, 50 ] )
+mlab.view(90, 90, 300, [ 50, 30, 50 ] )
 
 pitch = 1
 for id in c.label_ids:
 	vert, face,_ ,_ = create_surface(pitch, c.label_volume == id)
 	color = tuple(np.random.rand(3))
-	mlab.triangular_mesh(vert[:,0], vert[:,1], vert[:,2], face, color= color, opacity=0.3)
+	mlab.triangular_mesh(vert[:,0], vert[:,1], vert[:,2], face, color=color, opacity=0.3)
 
 vert, face, _, _ = create_surface(pitch, c.ref_volume ^ (c.label_volume > 0))
 color = (0.8,0.8,0.8)
