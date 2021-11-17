2c52b647527401b79352ec08efc8267cdc00d2d4
urakubo <hurakubo@gmail.com>
2021-11-17 23:16:27 +0900

    BuildAnyShape

--- pyLD/BuildAnyShape.py
+++ pyLD/BuildAnyShape.py
@@ -3,7 +3,7 @@ from __future__ import division
 import numpy as np
 import os
 import random
-
+from .utils import uM_to_num
 NUMPY_INTEGERS = [ np.int8, np.int16, np.int32, np.int64, \
 	np.uint8, np.uint16, np.uint32, np.uint64]
 
@@ -48,12 +48,6 @@ class BuildAnyShape:
 		ynum   = min([y_lsize, y_vsize])
 		znum   = min([z_lsize, z_vsize])
 
-                
-		# unit converter
-		spacing = sim.latticeSpacing
-		volume_in_L    = spacing * spacing * spacing * 1000 * x_lsize * y_lsize * z_lsize
-		NA             = 6.022e23
-		self.uM_to_num = 1e6 / NA / volume_in_L # umol-1 per L => mol-1 per L
 
 		# Set volume
 		# Can be accelerated using serialization.
@@ -80,8 +74,16 @@ class BuildAnyShape:
 		self.num_voxels = dict.fromkeys(domains)
 		for domain_name in domains:
 		    self.num_voxels[domain_name] = len(self.locs[domain_name])
+                
+		# unit converter
+		NA           = 6.022e23
+		self.spacing = sim.latticeSpacing
+		experiment_volume_in_L = self.spacing * self.spacing * self.spacing * \
+                                         1000 * x_lsize * y_lsize * z_lsize
+		self.per_uM = 1/(NA*1e-6*experiment_volume_in_L)
 
 
+                
 		# Extract the regions of membranes and PSDs
 		
 		self.surf_voxel_locs = {}
@@ -98,6 +100,14 @@ class BuildAnyShape:
 
 
                 
+	def number_per_1uM(self, domain_name ):
+		num_voxels = self.num_voxels[domain_name]
+		spacing_in_m = self.spacing
+		conc_in_uM = 1
+		number_per_1uM = uM_to_num(conc_in_uM, num_voxels, spacing_in_m)
+		return number_per_1uM
+
+                
 	def define_species(self, molecular_names):
 		# Check arguments
 		if isinstance(molecular_names, str):
@@ -109,21 +119,41 @@ class BuildAnyShape:
 
 	def modify_region(self, domain_name ):
 		#
-                return self.sim.modifyRegion( domain_name )
+		return self.sim.modifyRegion( domain_name )
 
 	def modifyRegion(self, domain_name ):
 		#
-                return self.sim.modifyRegion( domain_name )
+		return self.sim.modifyRegion( domain_name )
 
 	def add_reaction(self, reactant, product, rate, domain_name ):
 		#
-                self.sim.modifyRegion( domain_name ).addReaction(reactant=reactant, product=product, rate=rate)
+		self.sim.modifyRegion( domain_name ).addReaction(reactant=reactant, product=product, rate=rate)
+
+	def reac_oneway_uM(self, reactant, product, rate, domain_name ):
+		if len(reactant) == 2:
+			self.sim.modifyRegion(domain_name).addReaction(reactant=reactant, product=product, rate=rate*self.per_uM)
+		else:
+			self.sim.modifyRegion(domain_name).addReaction(reactant=reactant, product=product, rate=rate)
+
+	def reac21_uM(self, reac1, reac2, prod, kf, kb, domain_name ):
+		self.sim.modifyRegion(domain_name)\
+				.addReaction(reactant=(reac1, reac2), product=prod, rate=kf*self.per_uM)
+				.addReaction(reactant=prod, product=(reac1, reac2), rate=kb)
+
+	def reac12_uM(self, reac, prod1, prod2, kon, koff, domain_name ):
+		self.sim.modifyRegion(domain_name)\
+				.addReaction(reactant=reac, product=(prod1, prod2), rate=kf)
+				.addReaction(reactant=(prod1, prod2), product=reac, rate=kb*self.per_uM)
 
 	def set_diffusion_rate(self, molecular_name, rate, domain_name ):
 		#
-                self.sim.modifyRegion( domain_name ).setDiffusionRate(molecular_name, rate=rate)
+		self.sim.modifyRegion( domain_name ).setDiffusionRate(molecular_name, rate=rate)
 
-                
+	def add_solute_molecules_uM(self, molecular_name, number, domain_name):
+		#
+		number_per_1uM = self.number_per_1uM(domain_name)
+		self.add_solute_molecules( molecular_name, int(number*number_per_1uM), domain_name )
+
 	def add_solute_molecules(self, molecular_name, number, domain_name):
 		"""Add solute molecules.
 
@@ -135,7 +165,6 @@ class BuildAnyShape:
 		Returns:
 			(bool): The return value. True for success, False otherwise.
 		"""
-
 		particleNum=self.sim.particleMap[molecular_name]
 		for i in random.sample(self.locs[domain_name], number):
 		    self.sim.lattice.addParticle(i[0], i[1], i[2], int(particleNum))
---
+++ tutorial/1/main_build2.py
@@ -0,0 +1,102 @@
+import os
+import numpy as np
+import h5py
+from pyLM import *
+from pyLM.units import *
+from pyLD import * 
+
+
+input_filename_morph = 'models/ball_and_stick.h5'
+output_filename_lm   = 'models/ball_and_stick_AB_C.lm'
+
+ext  = 'default'
+cyt  = 'cytoplasm'
+er   = 'er'
+mito = 'mito'
+domains = {ext: 0, cyt: 1, er: 2, mito: 3}
+
+
+
+class setMolecules:
+    def __init__(self, cell):
+        self.cell     = cell
+        self.cyt_mol  = ['A','B','C']
+        self.cell.define_species(self.cyt_mol)
+
+    def addMolecules(self, domain_name):
+        conc = 1 # uM
+        conc_A = conc
+        conc_B = conc * 100
+        conc_C = 0
+        cell.add_solute_molecules_uM( 'A', conc_A, domain_name ) # Absolute number
+        cell.add_solute_molecules_uM( 'B', conc_B, domain_name ) # Absolute number
+        cell.add_solute_molecules_uM( 'C', conc_C, domain_name ) # Absolute number
+
+    def setDiffusion(self, domain_name):
+        d_ABC = 10 * 1e-12 # Diffusion constant
+        self.d_mol = {'A':d_ABC ,
+                      'B':d_ABC ,
+                      'C':d_ABC }
+        for k, v in self.d_mol.items():
+            self.cell.set_diffusion_rate(k, v, domain_name)
+
+    def setReactions(self, domain_name):
+        # uM_s_to_number_s
+        on = self.cell.per_uM
+
+        # Ca-CaM binding reactions
+        kon_AB_C = 1 * on / 100
+        kof_AB_C = 1 * 1
+
+        # AB binding to C
+        A = 'A'
+        B = 'B'
+        C = 'C'
+        cyt = self.cell.modifyRegion( domain_name )
+        cyt.addReaction(reactant=(A,B), product=C    , rate=kon_AB_C)
+        cyt.addReaction(reactant= C   , product=(A,B), rate=kof_AB_C)
+
+
+
+print('\nLoad geometry data.\n')
+
+with h5py.File(input_filename_morph,'r') as r:
+    dendrite_not_mitochondrion_not_ER = r['dendrite not mitochondrion not ER'][()]
+    pitch          = r['unit length per voxel (um)'][()]
+    PSD            = r['PSD'][()]
+    vol_bound      = r['boundary areas in volume'][()]
+
+
+volume = (dendrite_not_mitochondrion_not_ER > 0) * domains[cyt]
+vol_PSD  = vol_bound * (PSD > 0)
+surfaces = {'PSD': vol_PSD, 'cell boundary': vol_bound}
+
+
+print('\nBuild a lm model.\n')
+spacing=micron(pitch) # Units in SI
+nx,ny,nz = volume.shape
+sim  = RDME.RDMESimulation(dimensions=micron(nx*pitch,ny*pitch,nz*pitch), spacing=spacing)
+cell = BuildAnyShape(sim, volume, domains, surfaces)
+
+num_voxel_cyt  = np.count_nonzero(volume == domains[cyt])
+number_per_1uM = uM_to_num(conc_in_uM = 1, num_voxels = num_voxel_cyt, spacing_in_m = spacing)
+print('Number of molecules per 1 uM:', number_per_1uM)
+
+
+print('\nSet molecules.\n')
+molecules = setMolecules(cell)
+molecules.addMolecules(cyt)
+molecules.setDiffusion(cyt)
+molecules.setReactions(cyt)
+
+
+print('\nSet time.\n')
+sim.setTimestep(microsecond(3.0))
+sim.setWriteInterval(0.05)
+sim.setLatticeWriteInterval(0.05)
+sim.setSimulationTime(5.0)
+
+print('\nSave simulation setup.\n')
+if(os.path.isfile(output_filename_lm) == True):
+    os.remove(output_filename_lm)
+sim.save(output_filename_lm)
