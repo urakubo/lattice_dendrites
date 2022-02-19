c3584c84f3dc417c8eb07e0dea9d941003bb08f6
urakubo <hurakubo@gmail.com>
2022-02-19 20:12:34 +0900

    Docs

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('../'))
 # -- Project information -----------------------------------------------------
 
 project = 'lattice dendrites'
-copyright = '2021, Hidetoshi Urakubo'
+copyright = '2022, Hidetoshi Urakubo'
 author = 'Hidetoshi Urakubo'
 
 # The full version, including alpha/beta/rc tags
