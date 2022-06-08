149a7af9d7f8c4908161f24583d3d1d7b424a1c1
urakubo <hurakubo@gmail.com>
2022-06-08 19:08:40 +0900

    Packages

---
+++ Debian/README.txt
@@ -0,0 +1,37 @@
+Package: lattice-dendrites
+Version: 2.3.0.1
+Architecture: amd64
+Section: python
+Priority: optional
+Depends: libc6, libstdc++6, libgcc1, zlib1g, libexpat1
+Homepage: https://github.com/urakubo/lattice_dendrites
+Maintainer: Hidetoshi Urakubo
+Description: This is a part of lattice microbes 2.3.0.1
+  (Zhaleh Ghaemi version, the Luthey-Schulten Group)
+  as a part of lattice dendrites.
+  It works on Ubuntu (18.04 confirmed). It contains:
+  /usr/bin
+     lm
+     lm_python
+     lm_setdm
+     lm_setp
+     lm_setrm
+  /usr/lib
+     libhdf5.settings
+     libhdf5.so.200
+     libhdf5_hl.so.200
+
+
+Copyrights for Lattice Microbes are held by the Luthey-Schulten Group under the University of Illinois Open Source License.
+1) URLs of Lattice Microbes
+http://faculty.scs.illinois.edu/schulten/lm/
+http://faculty.scs.illinois.edu/schulten/Software2.0.html#1
+
+2) University of Illinois Open Source License.
+http://faculty.scs.illinois.edu/schulten/lm/LICENSE
+
+The Illinois Open Source License allows the redistributions of source code as far as it retains the copyright notice. Hidetoshi Urakubo (HU) thus re-distribute its version v2.3.0 with the patches by Dr. Zhaleh Ghaemi in this directory. 
+
+Hidetoshi Urakubo, 2022/2/22
+
+
--- setup.py
+++ setup.py
@@ -3,51 +3,58 @@ from setuptools.command.develop import develop
 from setuptools.command.install import install
 
 import os, sys
-import tarfile
-from pyLD.check_cuda import locate_cuda
 
 
 with open("README.md") as f:
 	long_description = f.read()
 
-class PostInstallCommand(install):
-	"""Post-installation for installation mode."""
-	def run(self):
-		install.run(self)
-		# PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
-		# print(self.install_lib)
+claasifiers = [
+		"License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+		"Programming Language :: Python",
+		"Topic :: Scientific/Engineering",
+		"Intended Audience :: Science/Research"
+	]
 
+packages    = ['pyLM','pySTDLM','pyLD','lm']
+package_dir = {}
 
-class PostDevelopCommand(develop):
-	"""Post-installation for development mode."""
-	def run(self):
-		develop.run(self)
-		# PUT YOUR POST-INSTALL SCRIPT HERE or CALL A FUNCTION
+major_version = sys.version_info[0] # Major
+minor_version = sys.version_info[1] # Minor
+
+print("Python{} on {} is detected.".format(sys.version_info, os.name))
+if os.name == 'posix' and major_version == 3 and major_version == 6:
+	claasifiers.extend(["Programming Language :: Python :: 3.6",
+		"Operating System :: POSIX"])
+	package_dir ={'lm': 'LM3.6'}
+	python_requires='3.6'
+
+elif os.name == 'posix' and major_version == 3 and major_version == 10:
+	claasifiers.extend(["Programming Language :: Python :: 3.8",
+		"Operating System :: POSIX"])
+	package_dir ={'lm': 'LM3.10'}
+	python_requires='3.10'
+
+elif major_version == 3:
+	claasifiers.extend(["Programming Language :: Python :: 3",
+		"Operating System :: Microsoft :: Windows :: Windows 10"])
+	package_dir ={'lm': 'LM_null'}
+	python_requires='>=3.0'
+
+else :
+    sys.exit("This version and/or system is not supported.".format(sys.version_info, os.name))
+
 
 s = setup(
 	name="lattice_dendrites",
 	version="0.1.0",
-	author="hidetoshi-urakubo",
+	author="Hidetoshi Urakubo",
 	author_email="hurakubo@gmail.com",
 	description="Lattice dendrites",
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	include_package_data=True, # described in MANIFEST.in
 	url="https://github.com/urakubo/lattice_dendrites",
-	classifiers=[
-		"License :: OSI Approved :: MIT License",
-		"Programming Language :: Python",
-		"Programming Language :: Python :: 3",
-		"Programming Language :: Python :: 3.6",
-		"Programming Language :: Python :: 3.7",
-		"Programming Language :: Python :: 3.8",
-		"Programming Language :: Python :: 3.9",
-		"Topic :: Scientific/Engineering",
-		"Intended Audience :: Science/Research",
-		"Operating System :: POSIX",
-		"Operating System :: MacOS",
-		"Operating System :: Microsoft :: Windows :: Windows 10",
-	],
+	classifiers=classifiers,
 	install_requires=[
 		"h5py",
 		"numpy>=1.16.1",
@@ -57,19 +64,14 @@ s = setup(
 		"scikit-image>=0.17.2",
 		"trimesh>=3.9.36"
 	],
-	packages = ['pyLM','pySTDLM','pyLD'],
-	package_data={"lm": ['*']},
-	python_requires="~=3.6", # >= 3.6 < 4.0
-	cmdclass={
-	    'develop': PostDevelopCommand,
-	    'install': PostInstallCommand,
-	},
+	packages = ['pyLM','pySTDLM','pyLD','lm'],
+	package_dir = package_dir,
+	python_requires=python_requires,
 	entry_points={
 		'console_scripts': [
 		'check_lm_install_dir = pyLD:check_lm_install_dir',
 	],
 },
-
 )
 
 
